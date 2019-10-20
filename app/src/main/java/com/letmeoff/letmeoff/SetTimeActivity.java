ade94f0929c6a7d4a5ac91cfa2ec2c7628407b9b
Haemin Ryu <happygoals@naver.com>
2019-10-20 14:43:28 -0500

    Bus stop number picker fixed

--- app/src/main/java/com/letmeoff/letmeoff/SetTimeActivity.java
+++ app/src/main/java/com/letmeoff/letmeoff/SetTimeActivity.java
@@ -8,10 +8,10 @@ import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
 
-public class SetTimeActivity extends AppCompatActivity implements NumberPicker.OnValueChangeListener {
+public class SetTimeActivity extends AppCompatActivity implements NumberPicker.OnValueChangeListener{
 
     private TextView StopNumbers;
-    private Button button1;
+    private Button button;
 
 
     @Override
@@ -22,23 +22,28 @@ public class SetTimeActivity extends AppCompatActivity implements NumberPicker.O
         StopNumbers = findViewById(R.id.StopNumbers);
         NumberPicker numberPicker = findViewById(R.id.numberPicker);
         numberPicker.setMinValue(0);
-        numberPicker.setMinValue(5);
+        numberPicker.setMaxValue(5);
         numberPicker.setOnValueChangedListener(this);
 
-        button1 = (Button) findViewById(R.id.alarmbutton);
-        button1.setOnClickListener(new View.OnClickListener(){
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View v){
                 openActivity2();
             }
+
         });
     }
 
-    @Override
-    public void onValueChange(NumberPicker numberPicker, int i, int il){
-        StopNumbers.setText( i +" Stop(s) Before"+il);
+        @Override
+        public void onValueChange(NumberPicker numberPicker, int i, int il){
+            StopNumbers.setText((i+1) +" Stop(s) Before ");
 
-    }
+        }
+
+
+
 
     public void openActivity2(){
         Intent intent = new Intent(this, ConfirmActivity.class);
