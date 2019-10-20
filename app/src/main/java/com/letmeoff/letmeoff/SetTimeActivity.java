13c29f5167b6181f0547102f3496dd02c9774f83
Haemin Ryu <happygoals@naver.com>
2019-10-20 09:04:54 -0500

    create the stop select activity

--- app/src/main/java/com/letmeoff/letmeoff/SetTimeActivity.java
+++ app/src/main/java/com/letmeoff/letmeoff/SetTimeActivity.java
@@ -2,12 +2,30 @@ package com.letmeoff.letmeoff;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
-public class SetTimeActivity extends AppCompatActivity {
+public class SetTimeActivity extends AppCompatActivity implements NumberPicker.OnValueChangeListener {
+
+    private TextView StopNumbers;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_time);
+
+        StopNumbers = findViewById(R.id.StopNumbers);
+        NumberPicker numberPicker = findViewById(R.id.numberPicker);
+        numberPicker.setMinValue(0);
+        numberPicker.setMinValue(5);
+        numberPicker.setOnValueChangedListener(this);
+    }
+
+    @Override
+    public void onValueChange(NumberPicker numberPicker, int i, int il){
+        StopNumbers.setText( i +" Stop(s) Before"+il);
+
     }
 }
+
