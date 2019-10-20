542bb774fabd17e22094efee94075ea3d4213b84
Haemin Ryu <happygoals@naver.com>
2019-10-19 21:45:58 -0500

    Create the default screen with the logo

--- app/src/main/java/com/letmeoff/letmeoff/MainActivity.kt
+++ app/src/main/java/com/letmeoff/letmeoff/MainActivity.kt
@@ -2,11 +2,23 @@ package com.letmeoff.letmeoff
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*Add in Oncreate() funtion after setContentView()*/
+
     }
+
+    val simpleSwitch = findViewById<View>(R.id.simpleSwitch) as Switch // initiate Switch
+
+    simpleSwitch.textOn = "Time" // displayed text of the Switch whenever it is in checked or on state
+    simpleSwitch.textOff = "Bus Stop"
+
 }
