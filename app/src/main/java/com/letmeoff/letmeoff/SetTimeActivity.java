cd541fd2dbf83fbc302ed2fad9e1f1bf0b2279d8
Haemin Ryu <happygoals@naver.com>
2019-10-20 09:36:14 -0500

    Create confirm activity with interaction with SetAlertActivity

--- app/src/main/java/com/letmeoff/letmeoff/SetTimeActivity.java
+++ app/src/main/java/com/letmeoff/letmeoff/SetTimeActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class SetTimeActivity extends AppCompatActivity implements NumberPicker.OnValueChangeListener {
 
     private TextView StopNumbers;
+    private Button button1;
 
 
     @Override
@@ -20,6 +24,14 @@ public class SetTimeActivity extends AppCompatActivity implements NumberPicker.O
         numberPicker.setMinValue(0);
         numberPicker.setMinValue(5);
         numberPicker.setOnValueChangedListener(this);
+
+        button1 = (Button) findViewById(R.id.alarmbutton);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openActivity2();
+            }
+        });
     }
 
     @Override
@@ -27,5 +39,11 @@ public class SetTimeActivity extends AppCompatActivity implements NumberPicker.O
         StopNumbers.setText( i +" Stop(s) Before"+il);
 
     }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, ConfirmActivity.class);
+        startActivity(intent);
+    }
+
 }
 
